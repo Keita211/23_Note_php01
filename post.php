3498297c9026cd39e23d9fae4fb0845238c94001
Keita Note <keitanote211@gmail.com>
2021-06-13 13:11:56 +0900

    add input form

---
+++ post.php
@@ -0,0 +1,25 @@
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>アンケート入力</title>
+</head>
+<body>
+
+<h1>脱炭素診断</h1>
+
+<form action="write.php" method="post">
+お名前：<input type="text" name="name"><br>
+EMAIL：<input type="text" name="mail"><br>
+脱炭素のために一ヶ月に支払っても良い金額：<input type="number" name="money"><br><br>
+ <input type="submit" value="送信">
+ 
+
+</form>
+
+
+</body>
+
+</html>
+
+
